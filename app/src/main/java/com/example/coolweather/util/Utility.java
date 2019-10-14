0355fab9a50cd4b46793537874e708a50e8a5ec0
Chen Yao <chenyao086@tju.edu.cn>
2019-10-14 15:11:26 +0800

    完成遍历省市县三级列表功能

---
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -0,0 +1,77 @@
+package com.example.coolweather.util;
+
+import android.text.TextUtils;
+
+import com.example.coolweather.db.City;
+import com.example.coolweather.db.County;
+import com.example.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by 曜爸 on 2019/10/14.
+ */
+public class Utility {
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i=0;i<allProvinces.length();i++){
+                    JSONObject jsonObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(jsonObject.getString("name"));
+                    province.setProvinceCode(jsonObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCitys = new JSONArray(response);
+                for(int i=0;i<allCitys.length();i++){
+                    JSONObject jsonObject = allCitys.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(jsonObject.getString("name"));
+                    city.setCityCode(jsonObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public static boolean handleCountyResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCountys = new JSONArray(response);
+                for(int i=0;i<allCountys.length();i++){
+                    JSONObject jsonObject = allCountys.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(jsonObject.getString("name"));
+                    county.setWeatherId(jsonObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+}
