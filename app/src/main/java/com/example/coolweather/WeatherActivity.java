a25a8efc4bb5a25bb626f7cb08ff3e4b34b81ef1
Chen Yao <chenyao086@tju.edu.cn>
2019-10-15 15:16:37 +0800

    修改程序图标和名称

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -156,7 +156,7 @@ public class WeatherActivity extends AppCompatActivity {
     private void showWeatherInfo(Weather weather){
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split(" ")[1];
-        String degree = weather.now.temperature + "℃";
+        String degree = weather.now.temperature + getString(R.string.temperature);
         String weatherInfo = weather.now.more.info;
         titleCity.setText(cityName);
         titleUpdateTime.setText(updateTime);
@@ -180,9 +180,9 @@ public class WeatherActivity extends AppCompatActivity {
             aqiText.setText(weather.aqi.city.aqi);
             pm25Text.setText(weather.aqi.city.pm25);
         }
-        String comfort = "舒适度：" + weather.suggestion.comfort.info;
-        String carWash = "洗车指数：" + weather.suggestion.carWash.info;
-        String sport = "运行建议：" + weather.suggestion.sport.info;
+        String comfort = getString(R.string.comfort) + weather.suggestion.comfort.info;
+        String carWash = getString(R.string.wash_car) + weather.suggestion.carWash.info;
+        String sport = getString(R.string.suggestion) + weather.suggestion.sport.info;
         comfortText.setText(comfort);
         carWashText.setText(carWash);
         sportText.setText(sport);
